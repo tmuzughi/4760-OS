554e20d70677a79af1f960adbbe03cbe2f29ee87
tmuzughi <55818129+tmuzughi@users.noreply.github.com>
2020-02-21 00:53:22 -0600

    Forking execs
    *Tested shared memory
    *Tested forking and exec

--- 4760 Ass2/README.txt
+++ 4760 Ass2/README.txt
@@ -1,1 +1,11 @@
 # 4760-OS
+
+Git log:
+2/19/20
+*Created .c files
+*Created Makefile
+*Added and tested options
+
+2/20/20
+*Tested allocating and deallocating shared memory
+*Tested forking and exec
--- 4760 Ass2/oss.c
+++ 4760 Ass2/oss.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
 
 int main(int argc, char* argv[]){
 
@@ -48,7 +50,7 @@ switch(options){
 
 
 /* test argv input with options */
-
+/*
 int count = argc;
 int i;
 
@@ -58,11 +60,66 @@ printf("%d\n", bValue);
 printf("%d\n", iValue);
 printf("%s\n", outputFile);
 
-//printf("%d", argc);
-
-/*for (i = 0; i < count; i++){
-	printf("%s", argv[i]);
-}
 */
+
+/* Test shared memory allocation/deallocation */
+/*
+// ftok to generate unique key
+//key_t key = ftok("shmfile", 65);
+
+// shmget returns an identifier in shmid
+int shmid = shmget(IPC_PRIVATE, 1024, 0666|IPC_CREAT);
+
+// error check for shmget
+if (shmid < 0){
+	printf("shmget error\n");
+	exit(1);
+}
+
+// shmat to attach to shared memory
+char *str = (char*) shmat(shmid, (void*) 0, 0);
+
+str = "dog";
+
+printf("%s\n", str);
+
+// detach from shared memory
+shmdt(str);
+
+//printf("%s\n", str);
+
+//remove shared memory segment
+shmctl(shmid, IPC_RMID, NULL);
+
+//try to pull from shared memory (should fail)
+printf("%s", str);
+
+*/
+
+/* Test forking */
+
+int pid = fork();
+
+if (pid < 0){
+	printf("There was a forking error.\n");
+	exit(1);
+}
+else if (pid == 0){
+	char *args[] = {"./prime", NULL};
+	execvp(args[0], args); 
+}
+else{
+	wait();
+	printf("Child PID is %d.\n", pid);
+}
+
+
+
+
+
+
+
+
+
 return 0;
 }
