8769dba91f25151eae01af4d5ecbba0b46da7ab0
tmuzughi <55818129+tmuzughi@users.noreply.github.com>
2020-02-24 17:04:54 -0600

    update 2/24/20

--- 4760 Ass2/oss.c
+++ 4760 Ass2/oss.c
@@ -5,12 +5,15 @@
 #include <sys/types.h>
 #include <sys/ipc.h>
 #include <sys/shm.h>
+#include <time.h>
 
 
 
 int main(int argc, char* argv[]){
 
-	
+	/*Start 'real time' clock*/
+	clock_t start, check;
+	start = clock();
 
 int options;
 int nValue = 4;
@@ -53,7 +56,9 @@ switch(options){
 	}
 }
 
-
+/*---------------------------------------
+	Test integer in shared memory
+----------------------------------------*/
 // shmget returns an identifier in shmid
 int shmid = shmget(123, 1024, 0666|IPC_CREAT);
 
@@ -69,12 +74,56 @@ int *pint = (int*)(paddr);
 *pint = 10;
 // detach from shared memory
 shmdt(paddr);
+/*---------------------------------------*/
 
-/*!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!*/
+/*---------------------------------------
+	Make seconds integer for clock
+		in shared memory
+----------------------------------------*/
+// shmget returns an identifier in shmid
+int shmidCs = shmget(456, 1024, 0666 | IPC_CREAT);
+
+// error check for shmget
+if (shmidCs < 0) {
+	printf("shmget error\n");
+	exit(1);
+}
 
 // shmat to attach to shared memory
-int* array;
+char* paddrCs = (char*)shmat(shmidCs, NULL, 0);
+int* seconds = (int*)(paddrCs);
+*seconds = 0;
+// detach from shared memory
+//shmdt(seconds);
+/*---------------------------------------*/
 
+/*---------------------------------------
+	Make nano seconds integer for clock
+		in shared memory
+----------------------------------------*/
+// shmget returns an identifier in shmid
+int shmidCn = shmget(789, 1024, 0666 | IPC_CREAT);
+
+// error check for shmget
+if (shmidCn < 0) {
+	printf("shmget error\n");
+	exit(1);
+}
+
+// shmat to attach to shared memory
+char* paddrCn = (char*)shmat(shmidCn, NULL, 0);
+long int* nanoSeconds = (long int*)(paddrCn);
+*nanoSeconds = 0;
+// detach from shared memory
+//shmdt(seconds);
+/*---------------------------------------*/
+
+
+/*--------------------------------------
+	Shared memory flag array
+---------------------------------------*/
+// shmat to attach to shared memory
+int* array;
 // shmget returns an identifier in shmid
 int shmidAr = shmget(0101, 1024, 0666 | IPC_CREAT);
 // error check for shmget
@@ -89,18 +138,48 @@ for (i = 0; i < 21; i++) {
 }
 // detach from shared memory
 shmdt(array);
-
+/*-------------------------------------*/
 
 int numberOfChildrenCreated = 0;
 int childrenWorking = 0;
 /* Test forking */
 int pid;
+/*--------------------------------------
+			   THE LOOP
+---------------------------------------*/
 while (numberOfChildrenCreated <= nValue) {
-	//check to see if a process has termiinated
+	/*------------------------------------
+				Real Clock
+	-------------------------------------*/
+	double totalTime;
+	check = clock();
+	totalTime = ((double)(check - start) / CLOCKS_PER_SEC);
+	if (totalTime > 2)
+	{
+		printf("Process oss terminated: elapsed time greater than 2 seconds");
+		exit(1);
+	}
+	/*------------------------------------
+			  Simulated Clock
+	-------------------------------------*/
+	long int nanoSecondsHand = *nanoSeconds;
+	nanoSecondsHand += 10000;
+	*nanoSeconds = nanoSecondsHand;
+	/*
+	if (nanoSecondsHand > 1000000000) {
+		int secondsHand = *seconds;
+		secondsHand++;
+		*seconds = secondsHand;
+
+		nanoSecondsHand = (nanoSecondsHand - 1000000000);
+		*nanoSeconds = nanoSecondsHand;
+	}
+	*/
 	
+	/*check to see if a process has termiinated
+		using an array of shared memory flags*/	
 	if (numberOfChildrenCreated >= sValue) {
 		int* arrayC;
-		//printf("%d\n", numberOfChildrenCreated);
 		int shmidArC = shmget(0101, 1024, 066);
 		if (shmidArC == -1) {
 			printf("error getting shmid\n");
@@ -111,26 +190,23 @@ while (numberOfChildrenCreated <= nValue) {
 		for (i = 0; i < 21; i++) {
 			if (arrayC[i] == 1) {
 				childrenWorking--;
-				arrayC[i] = 0;
-				
+				arrayC[i] = 0;			
 			}
-
-		}
-		
-		
+		}				
 	}
+	/*If all children have died break loop*/
 	if (numberOfChildrenCreated >= nValue && childrenWorking == 0)
 		break;
+	/*If max children have been created but some children are
+		still working, then restart loop*/
 	if (numberOfChildrenCreated >= nValue)
 		continue;
 
 	if (childrenWorking < sValue) {
 
-
 		pid = fork();
 		numberOfChildrenCreated++;
 		childrenWorking++;
-
 
 		if (pid < 0) {
 			printf("There was a forking error.\n");
@@ -153,20 +229,21 @@ while (numberOfChildrenCreated <= nValue) {
 				printf("Error with exec().\n");
 				exit(1);
 			}
-
-		}
-		else
-		{
-			//wait();
-			//childrenWorking--;
-			//printf("Some child died.\n");
-		}
-
+		}		
 	}
 }
+/*Deallocate shared memory*/
 printf("Fork vim.\n");
 shmctl(shmid, IPC_RMID, NULL);
 shmctl(shmidAr, IPC_RMID, NULL);
+
+/*test clock*/
+printf("%d\n", *seconds);
+printf("%d\n", *nanoSeconds);
+shmdt(seconds);
+shmdt(nanoSeconds);
+shmctl(shmidCs, IPC_RMID, NULL);
+shmctl(shmidCn, IPC_RMID, NULL);
 
 return 0;
 }
--- 4760 Ass2/prime.c
+++ 4760 Ass2/prime.c
@@ -6,8 +6,20 @@
 #include <unistd.h>
 #include <stdlib.h>
 
-int main(int argc, char* argv[]){
+void terminator(int *now, int start) {
+	
+}
 
+int main(int argc, char* argv[]){
+	/*get starting time in nanoseconds*/
+	int shmidCn = shmget(789, 1024, 0666);
+	if (shmidCn == -1) {
+		printf("error getting shmid\n");
+		exit(1);
+	}
+	int* startingNano = (int*)shmat(shmidCn, NULL, 0);
+	int save = *startingNano;
+	printf("Starting time in nanoseconds is %d\n", *startingNano);
 //printf("I am prime.c\n");
 //printf("%d\n", argv[0]);
 printf("I am child %d.\n", atoi(argv[0]));
@@ -29,6 +41,16 @@ printf("I will check %d for primality.\n", atoi(argv[1]));
 printf("Child %d is done now.\n\n", atoi(argv[0]));
 
 
+int shmidNow = shmget(789, 1024, 0666);
+if (shmidNow == -1) {
+	printf("error getting shmid\n");
+	exit(1);
+}
+int* now = (int*)shmat(shmidNow, NULL, 0);
+printf("calculation is %d - %d which is %d\n", *now, save, (*now - save));
+
+
+
 int* array;
 
 int shmidAr = shmget(0101, 1024, 066);
@@ -41,6 +63,7 @@ array[atoi(argv[0])] = 1;
 
 shmdt(array);
 shmdt(pull);
+shmdt(startingNano);
 
 return 0;
 
